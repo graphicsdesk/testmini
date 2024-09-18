344a9e0c79983c06a9b8cec102a3b74fbb3bd851
Molly Durawa <mollydurawa@Mollys-MacBook-Pro-5.local>
2024-09-17 20:10:33 -0400

    test10

--- src/scripts/page.js
+++ src/scripts/page.js
@@ -22,7 +22,7 @@ function init() {
 const SECTION_MAIN_SELECTOR = 'div#fusion-app';
 
 // ARTICLE_SELECTOR is where Arc Publishing puts the actual article content
-const ARTICLE_SELECTOR = 'div#fusion-app > div > div, div#fusion-app > section > div:nth-child(4)' ;
+const ARTICLE_SELECTOR = 'div#fusion-app > div' ;
 
 function hoistArticle() {
   // Store nodes of interest
